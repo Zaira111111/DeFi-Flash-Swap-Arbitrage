ac7f0c0b30b1a262b87cc0aadd83fe4fb35247ce
Sharukh <135739979+Zaira111111@users.noreply.github.com>
2025-12-19 18:37:12 +0530

    Update FlashArbitrage.sol

--- contracts/FlashArbitrage.sol
+++ contracts/FlashArbitrage.sol
@@ -15,4 +15,4 @@ contract FlashArbitrage {
 
     function startArbitrage(address pairAddress, uint amountToBorrow) external {
         // Trigger Flash
-        IUniswapV2Pair(p
+        IUniswapV2Pai
