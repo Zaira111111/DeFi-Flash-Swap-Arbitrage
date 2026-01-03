4861caadaec18fca3e4957bb861e0e8772bb6895
Sharukh <135739979+Zaira111111@users.noreply.github.com>
2026-01-03 16:28:34 +0530

    Update FlashArbitrage.sol

--- contracts/FlashArbitrage.sol
+++ contracts/FlashArbitrage.sol
@@ -8,7 +8,7 @@ contract FlashArbitrage {yy
     // Uniswap call back function
     function uniswapV2Call(address sender, uint amount0, uint amount1, bytes calldata data) external {
         // 1. Check if the call hycame from a real Uniswap Pair
-        // 2. Logic to Swap on Sushiswaphyh
+        // 2. Logic to Swap hhon Sushiswaphyh
         // 3. Logic to check Profitghh
         // 4. Repay Uniswa
     function startArbitrage(address pairAddresggys, uint amountToBorrow) external {
